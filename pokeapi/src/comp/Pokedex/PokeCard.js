422b0cc838dd715007693f1ed02eebfc885e2e07
Xylight0 <greppn@gmail.com>
2022-02-10 16:06:10 +0100

    working fetch

--- pokeapi/src/comp/Pokedex/PokeCard.js
+++ pokeapi/src/comp/Pokedex/PokeCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-export default function PokeCard({ pokeList }) {
+export default function PokeCard({ pokeList, pokeIndex }) {
   function createCard(index) {
-    console.log(pokeList);
+    console.log(pokeList[index]);
     return (
-      <div className="bg-white w-80 h-1/2 rounded-lg shadow-2xl">
-        {pokeList[index]}
+      <div className="bg-white w-80 h-1/2 rounded-lg shadow-2xl p-8 text-3xl">
+        {1}
       </div>
     );
   }
 
-  return <>{createCard(1)}</>;
+  return <>{createCard(pokeIndex)}</>;
 }
--- pokeapi/src/comp/Pokedex/Pokedex.js
+++ pokeapi/src/comp/Pokedex/Pokedex.js
@@ -2,29 +2,35 @@ import React, { useEffect, useState } from "react";
 import PokeCard from "./PokeCard";
 
 export default function Pokedex() {
-  const [pokeList, setPokeList] = useState([1, 2, 3, 4, 5, 6]);
-  const [pokeIndex, setpokeIndex] = useState("0");
+  const [pokeList, setPokeList] = useState([]);
+  const [pokeIndex, setpokeIndex] = useState(0);
 
   useEffect(() => {
-    //setPokeList(getPokeList());
+    getPokeList();
   }, []);
 
-  /*function getPokeList() {
+  function getPokeList() {
     fetch("https://pokeapi.co/api/v2/pokemon")
       .then((res) => res.json())
       .then((val) => setPokeList(val.results));
-  }*/
+  }
 
   return (
     <div className="w-full h-full flex flex-col gap-8 justify-center items-center">
-      <PokeCard pokeList={pokeList} />
+      <PokeCard pokeList={pokeList} pokeIndex={pokeIndex} />
       <div className="flex flex-row gap-8 w-80 justify-between">
-        <div className="bg-white px-4 py-1 rounded-md flex justify-center items-center text-2xl font-semibold">
-          <p>Prev</p>
-        </div>
-        <div className="bg-white px-4 py-1 rounded-md flex justify-center items-center text-2xl font-semibold">
-          <p>Next</p>
-        </div>
+        <button
+          onClick={() => setpokeIndex(() => (pokeIndex > 0 ? pokeIndex - 1 : 0))}
+          className="bg-white px-4 py-1 rounded-md flex justify-center items-center text-2xl font-semibold"
+        >
+          Prev
+        </button>
+        <button
+          onClick={() => setpokeIndex(pokeIndex + 1)}
+          className="bg-white px-4 py-1 rounded-md flex justify-center items-center text-2xl font-semibold"
+        >
+          Next
+        </button>
       </div>
     </div>
   );
