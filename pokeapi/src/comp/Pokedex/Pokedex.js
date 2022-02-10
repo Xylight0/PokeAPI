87cf2d1f9d9dcfba5612d11fb9c8e5765277df22
Xylight0 <greppn@gmail.com>
2022-02-10 16:15:36 +0100

    title changes to pokemon name

--- pokeapi/src/comp/Pokedex/Pokedex.js
+++ pokeapi/src/comp/Pokedex/Pokedex.js
@@ -3,16 +3,23 @@ import PokeCard from "./PokeCard";
 
 export default function Pokedex() {
   const [pokeList, setPokeList] = useState([]);
-  const [pokeIndex, setpokeIndex] = useState(0);
+  const [pokeIndex, setpokeIndex] = useState(-1);
 
   useEffect(() => {
     getPokeList();
   }, []);
 
+  useEffect(() => {
+    let titleName = pokeList.length > 0 ? pokeList[pokeIndex].name.toUpperCase() : "Pokedex";
+    document.title = titleName;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pokeIndex]);
+
   function getPokeList() {
     fetch("https://pokeapi.co/api/v2/pokemon")
       .then((res) => res.json())
-      .then((val) => setPokeList(val.results));
+      .then((val) => setPokeList(val.results))
+      .then(() => setpokeIndex(0));
   }
 
   return (
@@ -20,7 +27,9 @@ export default function Pokedex() {
       <PokeCard pokeList={pokeList} pokeIndex={pokeIndex} />
       <div className="flex flex-row gap-8 w-80 justify-between">
         <button
-          onClick={() => setpokeIndex(() => (pokeIndex > 0 ? pokeIndex - 1 : 0))}
+          onClick={() =>
+            setpokeIndex(() => (pokeIndex > 0 ? pokeIndex - 1 : 0))
+          }
           className="bg-white px-4 py-1 rounded-md flex justify-center items-center text-2xl font-semibold"
         >
           Prev
