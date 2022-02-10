a5caddffca01638c660afd8ce53b628587a8f2d7
Xylight0 <greppn@gmail.com>
2022-02-10 14:55:02 +0100

    pokeCart

--- pokeapi/src/comp/Pokedex/Pokedex.js
+++ pokeapi/src/comp/Pokedex/Pokedex.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from "react";
 
 export default function Pokedex() {
+  const [pokeList, setPokeList] = useState([]);
 
-    const [pokeList, setPokeList] = useState([])
+  useEffect(() => {
+    setPokeList(getPokeList);
+  }, []);
 
-    useEffect(() => {
-      setPokeList(getPokeList);
-    }, [])
-    
   function getPokeList() {
     fetch("https://pokeapi.co/api/v2/pokemon")
       .then((response) => response.json())
       .then((data) => console.log(data.results));
   }
-  return <div>Pokedex</div>;
+
+  function createCart() {
+    return <div className="bg-white w-80 h-1/2 rounded-lg shadow-2xl"></div>
+  }
+
+  return <div className="w-full h-full flex justify-center items-center">{createCart()}</div>;
 }
