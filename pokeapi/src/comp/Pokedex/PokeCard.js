91f311192e0dec1e558f63f6b3b800f5250bc9bf
Xylight0 <greppn@gmail.com>
2022-02-10 18:46:13 +0100

    overall index

--- pokeapi/src/comp/Pokedex/PokeCard.js
+++ pokeapi/src/comp/Pokedex/PokeCard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-export default function PokeCard({ pokeList, pokeIndex }) {
+export default function PokeCard({ pokeList, pokeIndex, overallIndex }) {
   const [pokemon, setpokemon] = useState(null);
   const [loading, setloading] = useState(true);
 
@@ -12,26 +12,28 @@ export default function PokeCard({ pokeList, pokeIndex }) {
   }
   useEffect(() => {
     fetchPokemon(pokeList[pokeIndex].url);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pokeIndex]);
 
   function createCard(pokemon) {
-    console.log(pokemon)
     const pokImgUrl = pokemon.sprites.other.dream_world.front_default;
     return (
       <div className="flex flex-col gap-4 items-center">
-        
         <div className="w-52 h-52 flex justify-center items-center mt-2 border-4 border-slate-400 rounded-full p-8">
-          <img className="" src={pokImgUrl} alt="pokemon" />
+          <img className="max-h-fit" src={pokImgUrl} alt="pokemon" />
         </div>
-        <p className="w-full flex justify-center font-semibold mt-2">{pokemon.name.toUpperCase()}</p>
+        <p className="w-full flex justify-center font-semibold mt-2 text-slate-800">
+          {pokemon.name.toUpperCase()}
+        </p>
       </div>
     );
   }
 
   return (
-    <div className="bg-white w-80 h-1/2 rounded-lg shadow-2xl p-8 text-3xl relative">
-      <p className="absolute left-4 top-4 text-xl font-semibold">#{pokeIndex}</p>
+    <div>
+      <p className="absolute left-4 top-4 text-xl font-semibold text-slate-700">
+        #{overallIndex}
+      </p>
       {loading ? <p>Loading...</p> : createCard(pokemon)}
     </div>
   );
--- pokeapi/src/comp/Pokedex/Pokedex.js
+++ pokeapi/src/comp/Pokedex/Pokedex.js
@@ -4,23 +4,46 @@ import PokeCard from "./PokeCard";
 export default function Pokedex() {
   const [pokeList, setPokeList] = useState([]);
   const [pokeIndex, setpokeIndex] = useState(-1);
+  const [overallIndex, setoverallIndex] = useState(0);
   const [loading, setloading] = useState(true);
+  const [nextUrl, setnextUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [prevUrl, setprevUrl] = useState(null);
 
   useEffect(() => {
-    getPokeList();
+    getPokeList(nextUrl);
   }, []);
 
   useEffect(() => {
-    let titleName =
-      pokeList.length > 0 ? pokeList[pokeIndex].name.toUpperCase() : "Pokedex";
+    let titleName = !loading
+      ? pokeList[pokeIndex].name.toUpperCase()
+      : "Pokedex";
     document.title = titleName;
     // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pokeIndex, loading]);
+
+  useEffect(() => {
+    if (pokeIndex === 20) {
+      setloading(true);
+      getPokeList(nextUrl);
+      setpokeIndex(0);
+    }
+    /*if(pokeIndex === 0 && overallIndex > 1){
+        setloading(true);
+        getPokeList(prevUrl);
+        setpokeIndex(19);
+    }*/
   }, [pokeIndex]);
 
-  function getPokeList() {
-    fetch("https://pokeapi.co/api/v2/pokemon")
+  function getPokeList(url) {
+    fetch(url)
       .then((res) => res.json())
-      .then((val) => setPokeList(val.results))
+      .then((list) => {
+        setnextUrl(list.next);
+        setprevUrl(list.previous);
+        let results = list.results;
+        results.push({ name: "last" });
+        setPokeList(results);
+      })
       .then(() => setpokeIndex(0))
       .then(() => setloading(false));
   }
@@ -28,18 +51,30 @@ export default function Pokedex() {
   return (
     <div className="w-full h-full flex flex-col gap-8 justify-center items-center">
       <p className="text-white text-6xl">Pokédex</p>
-      {loading ? null : <PokeCard pokeList={pokeList} pokeIndex={pokeIndex} />}
+      <div className="bg-white w-80 h-1/2 rounded-lg shadow-2xl p-8 text-3xl relative">
+        {loading ? null : (
+          <PokeCard
+            pokeList={pokeList}
+            pokeIndex={pokeIndex}
+            overallIndex={overallIndex}
+          />
+        )}
+      </div>
       <div className="flex flex-row gap-8 w-80 justify-between">
         <button
-          onClick={() =>
-            setpokeIndex(() => (pokeIndex > 0 ? pokeIndex - 1 : 0))
-          }
+          onClick={() => {
+            setpokeIndex(() => (pokeIndex > 0 ? pokeIndex - 1 : 0));
+            setoverallIndex(() => (overallIndex > 0 ? overallIndex - 1 : 0));
+          }}
           className="bg-white px-4 py-1 rounded-md flex justify-center items-center text-2xl font-semibold"
         >
           Prev
         </button>
         <button
-          onClick={() => setpokeIndex(pokeIndex + 1)}
+          onClick={() => {
+            setpokeIndex(pokeIndex + 1);
+            setoverallIndex(overallIndex + 1);
+          }}
           className="bg-white px-4 py-1 rounded-md flex justify-center items-center text-2xl font-semibold"
         >
           Next
